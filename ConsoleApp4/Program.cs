b8c4fd6f51dc132dd2e38ba96e37dc50e3a33b7e
puechua <puechua@gmail.com>
2024-02-19 01:36:41 +0100

    Dodaj pliki projektów.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Reflection;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HasASecret keeper = new HasASecret();
+
+            FieldInfo[] fields = keeper.GetType().GetFields(
+                BindingFlags.NonPublic | BindingFlags.Instance);
+
+            foreach(FieldInfo field in fields)
+            {
+                Console.WriteLine(field.GetValue(keeper));
+            }
+        }
+    }
+}
